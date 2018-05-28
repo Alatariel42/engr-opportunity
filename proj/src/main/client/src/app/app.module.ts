42c338253ecf0dd0ba767d63b0d4cc524a3fec9e
hvlawren <hvlawren@ncsu.edu>
2018-05-28 13:00:04 -0400

    Set up UI skeleton

--- proj/src/main/client/src/app/app.module.ts
+++ proj/src/main/client/src/app/app.module.ts
@@ -1,16 +1,53 @@
+import { MaterialImportsModule } from './material-imports/material-imports.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { NewsComponent } from './news/news.component';
+import { GetInvolvedComponent } from './get-involved/get-involved.component';
+import { HeaderComponent } from './header/header.component';
 
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'get-involved',
+    component: GetInvolvedComponent
+  },
+  {
+    path: '',
+    redirectTo: '/about',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CurriculumComponent,
+    NewsComponent,
+    GetInvolvedComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MaterialImportsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
