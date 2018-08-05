af135591b7cbe919ecaa634b7938363f01598858
Lauren Siegel <lcsiege@gmail.com>
2018-08-05 11:54:00 -0400

    users

--- proj/src/main/java/eng/opp/domain/User.java
+++ proj/src/main/java/eng/opp/domain/User.java
@@ -1,5 +1,7 @@
 package eng.opp.domain;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class User {
 	
 	private Long id;
@@ -61,7 +63,7 @@ public class User {
 	}
 
 	public void setPassword(String password) {
-		this.password = BCrypt.hashpw(password);
+		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
 	}
 
 	public String getEmail() {
