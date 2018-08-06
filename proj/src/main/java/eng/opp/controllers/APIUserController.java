d77701920f207f3ddb4ca9828adb54364db73ead
Lauren Siegel <lcsiege@gmail.com>
2018-08-05 23:24:25 -0400

    user service and controller skeletons

--- proj/src/main/java/eng/opp/controllers/APIUserController.java
+++ proj/src/main/java/eng/opp/controllers/APIUserController.java
@@ -1,5 +1,31 @@
 package eng.opp.controllers;
 
+@RestController
+@RequestMapping(path="/login")
 public class APIUserController {
 
+  @PostMapping(path="/addUser")
+  public ResponseEntity<User> createUser() {
+
+  }
+
+  @PatchMapping(path="/updateEmail")
+  public ResponseEntity<User> updateUserEmail() {
+
+  }
+
+  @PatchMapping(path="/updatePassword")
+  public ResponseEntity<User> updateUserPassword() {
+
+  }
+
+  @PutMapping(path="/updateUser")
+  public ResponseEntity<User> updateUser() {
+
+  }
+
+  @DeleteMapping(path="/deleteUser")
+  public ResponseEntity<User> deleteUser() {
+    
+  }
 }
--- proj/src/main/java/eng/opp/services/UserService.java
+++ proj/src/main/java/eng/opp/services/UserService.java
@@ -1,6 +1,28 @@
 package eng.opp.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 @Service
 public class UserService {
 
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  public ResponseEntity<> add(User user) {
+
+  }
+
+  public ResponseEntity<> update(User user) {
+
+  }
+
+  public ResponseEntity<> delete(User user) {
+
+  }
 }
