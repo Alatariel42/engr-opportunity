1938861c81df0a770ff4d285664a16089e49d8ea
Lauren Siegel <lcsiege@gmail.com>
2018-08-09 21:15:48 -0400

    added datasource

--- proj/src/main/java/eng/opp/config/WebSecurityConfig.java
+++ proj/src/main/java/eng/opp/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package eng.opp.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +11,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:db.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private Environment env;
+
+  @Bean
+  public DataSource getDataSource() {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+    dataSource.setUrl(env.getProperty("mysql.jdbcUrl"));
+    dataSource.setUsername(env.getProperty("mysql.username"));
+    dataSource.setPassword(env.getProperty("mysql.password"));
+    return dataSource;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery("select username, password, enabled"
+            + " from users where username=?")
+        .passwordEncoder(new BCryptPasswordEncoder());
+  }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/about","/contact").permitAll().anyRequest().authenticated().and()
-		.formLogin().loginPage("/login").permitAll().and()
-		.logout().permitAll();
+    http.authorizeRequests().anyRequest().authenticated()
+    .and()
+    .httpBasic();
 	}
 
 }
