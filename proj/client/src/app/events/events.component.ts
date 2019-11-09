6b475bc024bb14c49c6f8f0f62823b509e642229
hvlawren <hvlawren@ncsu.edu>
2019-11-08 22:27:09 -0500

    Update pages

--- proj/client/src/app/events/events.component.ts
+++ proj/client/src/app/events/events.component.ts
@@ -2,6 +2,9 @@ import { EventSignupComponent } from './../event-signup/event-signup.component';
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { DomSanitizer } from '@angular/platform-browser';
+
+const gcalUrl = "https://calendar.google.com/calendar/b/1/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=bmNzdS5lZHVfZXJma3NvczFndmpvbXNrdnI2azlvdTcxdG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233366CC&showTitle=1&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0";
 
 @Component({
   selector: 'app-events',
@@ -12,8 +15,10 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 export class EventsComponent implements OnInit {
   options: any;
   calendarPlugins = [dayGridPlugin]; // important!
-  constructor(private dialogService: MatDialog) {}
+  googeCalendarUrl;
+  constructor(private dialogService: MatDialog, private sanitizer: DomSanitizer) {}
   ngOnInit() {
+     this.googeCalendarUrl = this.sanitizer.bypassSecurityTrustResourceUrl(gcalUrl);
      this.options = {
         editable: false,
         eventLimit: false,
@@ -26,8 +31,38 @@ export class EventsComponent implements OnInit {
           {
             id: 585,
             title: 'Family Promise',
-            start: '2019-08-28T18:30:00+05:00',
-            end: '2019-08-28T19:30:00+05:00'
+            start: '2019-09-02T17:30:00-05:00',
+            end: '2019-09-02T18:30:00-05:00'
+          },
+          {
+            id: 586,
+            title: 'Family Promise',
+            start: '2019-09-09T17:30:00-05:00',
+            end: '2019-09-09T18:30:00-05:00'
+          },
+          {
+            id: 587,
+            title: 'Family Promise',
+            start: '2019-09-16T17:30:00-05:00',
+            end: '2019-09-16T18:30:00-05:00'
+          },
+          {
+            id: 588,
+            title: 'Family Promise',
+            start: '2019-09-23T17:30:00-05:00',
+            end: '2019-09-23T18:30:00-05:00'
+          },
+          {
+            id: 589,
+            title: 'Family Promise',
+            start: '2019-09-30T17:30:00-05:00',
+            end: '2019-09-30T18:30:00-05:00'
+          },
+          {
+            id: 590,
+            title: 'Haven House',
+            start: '2019-09-28T08:30:00-05:00',
+            end: '2019-09-28T10:30:00-05:00'
           }
         ]
       };
